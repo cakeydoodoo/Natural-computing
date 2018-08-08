33553561624593d3a85df4be30b8dc06395a609c
U-BABY\Cakeydoodoo <tvu010@campus.goldsmiths.ac.uk>
2018-08-08 20:30:18 +0100

    another commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Movement();
+
+    }
+
+        void FixedUpdate()
+    {
+
+    }
+
+    void Movement()
+    {
+
+        Vector3 inputVelocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        inputVelocity = inputVelocity * movementSpeed * Time.deltaTime;
+        rb.MovePosition(transform.position + (transform.forward * inputVelocity.z) + (transform.right * inputVelocity.x));
+
+    }
+}
